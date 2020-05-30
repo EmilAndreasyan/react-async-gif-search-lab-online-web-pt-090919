c125851977332544ac0e0964cdd992f3614a6b57
Emil Andreasyan <emil.andreasyan@yahoo.com>
2020-05-30 02:12:46 +0000

    Automatically backed up by Learn

--- src/containers/GifListContainer.js
+++ src/containers/GifListContainer.js
@@ -2,14 +2,22 @@ import React from 'react';
 
 export default class GiftListContainer extends React.Component {
   this.state = {
-    
+    api: []
   }
   
   componentDidMount(){
     fetch('https://api.giphy.com/v1/gifs/search?q=dolphin&api_key=mIuSfstFtNcWfvBo9cjiaDeIf6YltBOU&rating=g')
     .then(resp => resp.json())
     .then(data => {
-      
+      this.setState({
+        api: data
+      })
     })
+  }
+  
+  render(){
+    return(
+      <div>{this.state.api}</div>
+      )
   }
 }
