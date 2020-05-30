e37b100c092478eba0ee224488953edec54d0f12
Emil Andreasyan <emil.andreasyan@yahoo.com>
2020-05-30 02:47:49 +0000

    Automatically backed up by Learn

--- src/components/GifSearch.js
+++ src/components/GifSearch.js
@@ -7,11 +7,12 @@ export default class GifSearch extends React.Component {
     password: ''
   }
   
+  handleChange = 
   
   render(){
     return (
       <form onSubmit={this.props.handleSubmit}>
-      <input type="text" id="username" value={this.state.username} on/>
+      <input type="text" id="username" value={this.state.username} onChange={this.handleChange}/>
       </form>
       )
   }
