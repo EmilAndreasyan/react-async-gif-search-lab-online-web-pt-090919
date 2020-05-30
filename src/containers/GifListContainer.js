1bac5356f9c7ad711a476a34fb73b17836fe5b5c
Emil Andreasyan <emil.andreasyan@yahoo.com>
2020-05-30 02:09:49 +0000

    Automatically backed up by Learn

--- src/containers/GifListContainer.js
+++ src/containers/GifListContainer.js
@@ -6,6 +6,10 @@ export default class GiftListContainer extends React.Component {
   }
   
   componentDidMount(){
-    fetch()
+    fetch('https://api.giphy.com/v1/gifs/search?q=dolphin&api_key=mIuSfstFtNcWfvBo9cjiaDeIf6YltBOU&rating=g')
+    .then(resp => resp.json())
+    .then(data => {
+      
+    })
   }
 }
