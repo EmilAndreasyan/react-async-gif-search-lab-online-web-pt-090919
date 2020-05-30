6ce5570b713a9d2f56f0baac092fb17685ea4104
Emil Andreasyan <emil.andreasyan@yahoo.com>
2020-05-30 03:21:12 +0000

    Automatically backed up by Learn

---
+++ src/components/GifList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default class GitList extends React.Component {
+  
+  render(){
+    return (
+      <div>
+      <ul>
+      <li></li>
+      </ul>
+      </div>
+      )
+  }
+}
